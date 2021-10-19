4db187e2358814511355a27bcc35a3c78c945c79
Ritik <freelancingritikchoure@gmail.com>
2021-10-19 16:49:36 +0530

    first Commit

---
+++ database/migrations/2021_10_19_105119_create_quotes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Quote;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->id();
+            $table->text('quote');
+            $table->timestamps();
+        });
+
+        Quote::create([
+            'quote' => "Hello World",
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotes');
+    }
+}
